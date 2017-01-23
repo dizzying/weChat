27e1bcdf4e93a52a962dc5ee3b94c432261fd024
LV <lv@hun8.com>
2017-01-23 10:15:23 +0800

    修改包名称

--- autoload.php
+++ autoload.php
@@ -1,16 +1,16 @@
 <?php
 
 /*
- * This file is part of the stoneworld/socialite.
+ * This file is part of the Dizzying/Wechat-ent.
  *
- * (c) overtrue <i@stoneworld.github.io>
+ * (c) Dizzying <dev@hun8.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
 
 spl_autoload_register(function ($class) {
-    if (false !== stripos($class, 'Stoneworld\Wechat')) {
+    if (false !== stripos($class, 'Dizzying\Wechat-ent')) {
         require_once __DIR__.'/src/'.str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 10)).'.php';
     }
 });
